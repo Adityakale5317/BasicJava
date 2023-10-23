b3d8c1b8515b65fe140d248ba12167337b8da196
Minfy <Minfy@DESKTOP-KB2S39B>
2023-10-23 22:58:03 +0530

    Updated Code

---
+++ src/Basic_Code/Even_Odd_Whileloop.java
@@ -0,0 +1,29 @@
+package Basic_Code;
+
+public class Even_Odd_Whileloop {
+
+	public static void main(String[] args) {
+		 {
+		        int num = 1;
+		        int endRange = 10; // Define the range of numbers you want to print (e.g., from 1 to 10)
+
+		        while (num <= endRange) {
+		            if (num % 2 == 0) {
+		                System.out.println(num + " is even");
+		            } else {
+		                System.out.println(num + " is odd");
+		            }
+		            num++;
+		        }
+		    }
+		
+		
+
+
+
+
+
+
+	}
+
+}
